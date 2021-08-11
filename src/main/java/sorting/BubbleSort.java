eb69356c863d2d565678e57640d07674606e4cc8
lucas <lucas@gobaco.com>
2021-08-10 19:16:01 -0700

    Added Insertion Sort

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -8,7 +8,6 @@ public class BubbleSort {
                 for (int j = 0; j < n-i-1; j++)
                     if (arr[j] > arr[j+1])
                     {
-                        // swap arr[j+1] and arr[j]
                         int temp = arr[j];
                         arr[j] = arr[j+1];
                         arr[j+1] = temp;
