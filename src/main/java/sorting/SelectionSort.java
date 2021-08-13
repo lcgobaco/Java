b6c4c4a255683335388e1c90e66e967a4d62774e
lucas <lucas@gobaco.com>
2021-08-12 19:55:53 -0700

    Added Selection Sort

---
+++ src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,38 @@
+package sorting;
+
+public class SelectionSort
+{
+    public static void sort(int arr[])
+    {
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        SelectionSort ob = new SelectionSort();
+        int arr[] = {64, 25, 12, 22, 11, 77, 92, 35, 30, 19};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
